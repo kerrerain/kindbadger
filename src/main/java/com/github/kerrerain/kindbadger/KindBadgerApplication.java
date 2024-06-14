9f579457e0ce7ccf0abb6f15ccd357c08fd90463
kerrerain <mail.henry.bonjour@gmail.com>
2024-06-14 08:52:50 +0200

    Initial commit

---
+++ src/main/java/com/github/kerrerain/kindbadger/KindBadgerApplication.java
@@ -0,0 +1,13 @@
+package com.github.kerrerain.kindbadger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindBadgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindBadgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kerrerain/kindbadger/KindBadgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kerrerain.kindbadger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindBadgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
